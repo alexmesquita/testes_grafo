b33dd11a44545370dab66442f4a59af8b9a1dc90
Álex Mesquita <alex.mesquita06@hotmail.com>
2015-01-07 12:40:36 -0200

    criação de grafo por matriz de adjacencia :baby:

---
+++ matriz_adjacencia.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#define HEIGHT 4
+#define WIDTH 4
+
+using namespace std;
+
+void print_grafo(std::vector< std::vector<bool> > grafo){
+	for (int i = 0; i < HEIGHT; ++i)
+	{
+		for(int j = 0; j < WIDTH; j++)
+		{
+			if(j){
+				cout << ", "; 
+			}
+			else{
+				cout << "[";
+			}
+
+			cout << grafo[i][j];
+		}
+		cout << "]" << endl;
+	}
+}
+
+int main() {
+	vector< vector<bool> > grafo;
+
+	// Set up sizes. (HEIGHT x WIDTH)
+	grafo.resize(HEIGHT);
+	for (int i = 0; i < HEIGHT; ++i)
+		grafo[i].resize(WIDTH);
+
+	for (int i = 0; i < HEIGHT; ++i)
+	{
+		for(int j = 0; j < WIDTH; j++)
+		{
+			if(j != i){
+				grafo[i][j] = true;
+			}
+			else{
+				grafo[i][j] = false;
+			}
+		}
+	}
+
+	print_grafo(grafo);
+
+	return 0;
+}
